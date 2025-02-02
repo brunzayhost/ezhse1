4922ad5b856eaa6bfd8ad02213d334b490802a6c
brunzayhost <brunzayhost@gmail.com>
2025-02-02 13:59:34 +0800

    `Update tests.py to use Django 3+ TestCase and add more test assertions`

--- code/books/pages/tests.py
+++ code/books/pages/tests.py
@@ -1,3 +1,13 @@
+# pages/tests.py
 from django.test import TestCase
+from django.urls import reverse
+from .views import HomePageView
 
-# Create your tests here.
+class HomepageTests(TestCase):
+    def test_homepage(self):
+        """Test that the homepage returns a 200 status code and uses the correct template."""
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'Home')
+        self.assertNotContains(response, 'Hi there! I should not be on the page.')
