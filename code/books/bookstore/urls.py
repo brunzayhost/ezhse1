2b21c24ee99fd1cef85b32e190ff48d1041b3ce6
brunzayhost <brunzayhost@gmail.com>
2025-02-02 11:59:36 +0800

    urls up

--- code/books/bookstore/urls.py
+++ code/books/bookstore/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_page_view  # Import the view
+from pages.views import HomePageView  # Import the view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page_view, name='home'),  # Add this line
+    path('', HomePageView.as_view(), name='home'),  # Add this line
 ]
