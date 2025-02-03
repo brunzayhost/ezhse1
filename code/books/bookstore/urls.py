bea742e4acd4cc35ef7688b4dd2104b8f5810ed5
brunzayhost <brunzayhost@gmail.com>
2025-02-03 23:40:14 +0800

    logout

--- code/books/bookstore/urls.py
+++ code/books/bookstore/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
 # from django.contrib.auth import views as auth_views # new
 
 urlpatterns = [
     # Django admin
     path('admin/', admin.site.urls),
     
-    # User manaherment
+    # User management
     path('accounts/', include('django.contrib.auth.urls')), # new
     
     # Local apps
     path('', include('pages.urls')), # new
-    
-    # logout redirect
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'), # new
 ]
