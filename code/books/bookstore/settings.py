88f5fdf52c1651a57f9d967d4cb7ccf3ceb5f266
brunzayhost <84755421+brunzayhost@users.noreply.github.com>
2025-02-12 07:01:39 +0800

    Update settings.py

--- code/books/bookstore/settings.py
+++ code/books/bookstore/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from environs import Env # new
+env = Env() # new
+env.read_env() # new
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
